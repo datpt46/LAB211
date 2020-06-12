f5e93bc1292007b12fc47a7e4cae75dc1495cd5e
Dat Pham <datpham123335@gmail.com>
2020-06-12 12:06:30 +0700

    add validation

---
+++ J1SP0071/src/entity/Task.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Task {
+    private String id;
+    private String taskTypeId;
+    private String requirementName;
+    private String date;
+    private double planFrom;
+    private double planTo;
+    private String assignee;
+    private String reviewer;
+
+    public Task(String id, String taskTypeId, String requirementName, String date, double planFrom, double planTo, String assignee, String reviewer) {
+        this.id = id;
+        this.taskTypeId = taskTypeId;
+        this.requirementName = requirementName;
+        this.date = date;
+        this.planFrom = planFrom;
+        this.planTo = planTo;
+        this.assignee = assignee;
+        this.reviewer = reviewer;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTaskTypeId() {
+        return taskTypeId;
+    }
+
+    public void setTaskTypeId(String taskTypeId) {
+        this.taskTypeId = taskTypeId;
+    }
+
+    public String getRequirementName() {
+        return requirementName;
+    }
+
+    public void setRequirementName(String requirementName) {
+        this.requirementName = requirementName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public double getPlanFrom() {
+        return planFrom;
+    }
+
+    public void setPlanFrom(double planFrom) {
+        this.planFrom = planFrom;
+    }
+
+    public double getPlanTo() {
+        return planTo;
+    }
+
+    public void setPlanTo(double planTo) {
+        this.planTo = planTo;
+    }
+
+    public String getAssignee() {
+        return assignee;
+    }
+
+    public void setAssignee(String assignee) {
+        this.assignee = assignee;
+    }
+
+    public String getReviewer() {
+        return reviewer;
+    }
+
+    public void setReviewer(String reviewer) {
+        this.reviewer = reviewer;
+    }
+    
+    
+}
---
+++ J1SP0071/src/validation/getInput.java
@@ -0,0 +1,128 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package validation;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Administrator
+ */
+public class getInput {
+
+    static final Scanner IN = new Scanner(System.in);
+    static final String REGEX_PLAN = "^\\d{1,2}\\.5|\\d{1,2}\\.0$";
+
+    public String getString() {
+        while (true) {
+            String res = IN.nextLine().trim();
+
+            if (!res.isEmpty()) {
+                return res;
+            } else {
+                System.out.println("you cannot enter empty!");
+            }
+        }
+    }
+
+    public int getInt(String message, int min, int max) {
+        boolean flag = true;
+        int number = 0;
+
+        while (flag) {
+            System.out.println(message);
+            try {
+                number = Integer.parseInt(getString());
+                if (number >= min && number <= max) {
+                    flag = false;
+                } else {
+                    System.out.println("number out of range!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Number invalid!");
+            }
+        }
+        return number;
+    }
+
+    public double getDouble(String message, int min, int max) {
+        boolean flag = true;
+        double number = 0;
+
+        while (flag) {
+            System.out.println(message);
+            try {
+                number = Double.parseDouble(getString());
+                if (number >= min && number <= max) {
+                    flag = false;
+                } else {
+                    System.out.println("number out of range!");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Number invalid!");
+            }
+        }
+        return number;
+    }
+
+    public String getTaskNameById(String message) {
+        while (true) {
+            System.out.println(message);
+            int taskTdType = getInt("enter task type 1-4: ", 1, 4);
+            switch (taskTdType) {
+                case 1:
+                    return "Code";
+                case 2:
+                    return "Test";
+                case 3:
+                    return "Design";
+                case 4:
+                    return "Review";
+            }
+        }
+    }
+
+    public String getDate(String message) {
+        while (true) {
+            try {
+                System.out.println(message);
+                String dateInput = getString();
+
+                SimpleDateFormat format = new SimpleDateFormat("dd-MM-yy");
+                Date date = format.parse(dateInput);
+
+                if (dateInput.equalsIgnoreCase(format.format(date))) {
+                    return dateInput;
+                } else {
+                    System.out.println("enter again: ");
+                }
+            } catch (NumberFormatException e) {
+               e.printStackTrace();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+       
+    
+    public double getPlaneTime(String message) {
+        boolean planRange = Double.parseDouble(REGEX_PLAN) >=8 && Double.parseDouble(REGEX_PLAN)<=17.5;
+        while(true){
+            System.out.println(message);
+            String plan = getString();
+            
+            if(plan.matches(REGEX_PLAN) && planRange){
+                return Double.parseDouble(plan);
+            } else {
+                System.out.println("please input again: ");
+            }
+        }
+    }
+}
